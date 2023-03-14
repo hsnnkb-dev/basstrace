633803fd892792b30053d56120c28072c6660133
hasan <hsnnkb.dev@gmail.com>
2023-03-14 18:47:53 +0000

    Add tailwind css to setup, Initialise and clean up React bloat

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="flex flex-row basis-0 justify-center hover:basis-1/2">
+      <h1 className="text-3xl font-bold underline m-4">Hello world!</h1>
+    </header>
   );
 }
 
